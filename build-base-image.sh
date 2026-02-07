e367178635c78bfcea1106e7f9a221663efa7408
zakiabdullahi <qable2121@gmail.com>
2026-02-07 21:10:59 +0300

    modifeid .github/workflows/deploy.yml  Dockerfile.lambda build-base-image.sh

--- build-base-image.sh
+++ build-base-image.sh
@@ -22,7 +22,7 @@
 set -e  # Exit on error
 
 # Configuration
-AWS_ACCOUNT_ID="685057748560"
+AWS_ACCOUNT_ID="803443341114"
 AWS_REGION="us-east-1"
 ECR_REGISTRY="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
 ECR_REPOSITORY="lambda-python-deps"
